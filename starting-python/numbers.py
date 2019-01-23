f1bef7bd71cea2fedb1ab6c2a141a9d4c5501f32
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-01-23 14:05:19 -0800

    more math practice

--- starting-python/numbers.py
+++ starting-python/numbers.py
@@ -5,3 +5,23 @@ print(2-1)
 print(2*2)
 
 print(3/2)
+
+print("modulors gives us the remainder of a division")
+
+print(50 % 5)
+
+print(23 % 2)
+
+print(20 % 2)
+
+print("powers")
+
+print(2 ** 3)
+
+print("order of operations, parentheses can determine which operations start first in a line of multiple operations")
+
+print(2 + 10 * 10 + 3)
+print("10 times 10 comes first in this example")
+
+print((2 + 10) * (10 + 3))
+print("the parentheses come first in this one")
